0a6aef40611a16c06ffc3093f2c3e5cfa3d4e96f
jane <jane.peru2021@gmail.com>
2024-02-12 15:59:47 -0500

    Módulo resuelto

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -19,7 +19,7 @@ const nuevaResta = 10 - 7 === 3;
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * 4 === 40;
 
-// Resuelve el siguiente problema matemático.
+// Resuelve el siguiente problema matemático..
 const nuevoModulo = 21 % 5 === 1;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
